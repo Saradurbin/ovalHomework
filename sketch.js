5dfd4988098c3cd038096eda8bd41c34d1818abb
Sara Durbin <sara_durbin@bullis.org>
2017-03-09 12:21:13 -0500

    Homework
    Homework being finished

---
+++ sketch.js
@@ -0,0 +1,13 @@
+function setup() {
+createCanvas(800,600);
+}
+var xLocation = 100;
+function draw() {
+rect(xLocation,100,50,50);
+xLocation = xLocation + 1;
+
+ if(xLocation > 100){
+     rect (150,200,100,100);
+xLocation=15);
+}
+}
